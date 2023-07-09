77f91e180d9b806d69164bc18f53109713446d7a
masharqah <59175914+masharqah@users.noreply.github.com>
2023-07-09 13:02:45 +0300

    Add files via upload

---
+++ Sll.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+  
+    AddAtHead(value) {
+      var newNode = new Node(value);
+  
+      if (this.head === null) {
+        this.head = newNode;
+      } else {
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+  
+      return this.head;
+    }
+    removeFront(value) {
+        if (this.head === null) {
+            return null;
+        }
+        var currentHead = this.head;
+        this.head = this.head.next;
+    return this.head;
+  }
+    front(value){
+        if (this.head === null){
+            return null;}
+        return this.head.value;
+    }
+    }
+  
+SLL= new LinkedList();
+SLL.AddAtHead(5);
+SLL.AddAtHead(6);
+SLL.AddAtHead(100);
+console.log(SLL);
+SLL.removeFront();
+console.log(SLL);
+console.log(SLL.front());
+
